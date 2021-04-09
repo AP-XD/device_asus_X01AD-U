16de20bbc36b784a0a40eb9043930b2a00ee5767
AP-XD <arkamtg.pramanik@gmail.com>
2021-04-09 22:12:17 +0530

    X01AD: Ship Gcam Go

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor \
-	Snap
+    GCamGOPrebuilt
     
 # CNE
 PRODUCT_PACKAGES += \
